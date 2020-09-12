e8646d89f1193b46cfa818757d67e0aad7395949
Daniil-Tumanov <71175999+Daniil-Tumanov@users.noreply.github.com>
2020-09-12 22:14:05 +0300

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="shortcut icon" href="img/favicon.svg" type="image/x-icon">
+    <title>Контакты</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Russo One' rel='stylesheet'>
+</head>
+<?php require 'header.php';?>
+
+<div class="content">
+<p class="head">Контакты</p>
+<div class="contacts">
+    <p>Директор:</p> 
+        <p class="desc">Васильев В.В.</p><br>
+    <p>E-mail:</h2> 
+        <p class="desc">computer.shop@gmail.com</p><br>
+    <p>Телефон:</p> 
+        <p class="desc">8-988-888-88-88</p><br>
+    <p>Юр. Адрес:</p> 
+        <p class="desc"> 355000 Г.Ставрополь ул. Кулакова 2</p><br>
+</div>
+<div class="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1409.6095501265681!2d41.9091259936329!3d45.04077553428243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f9a9f9a623a2fb%3A0x561b13b560c3f9dd!2z0L_RgC3RgiDQmtGD0LvQsNC60L7QstCwLCAyLCDQodGC0LDQstGA0L7Qv9C-0LvRjCwg0KHRgtCw0LLRgNC-0L_QvtC70YzRgdC60LjQuSDQutGA0LDQuSwgMzU1MDI4!5e0!3m2!1sru!2sru!4v1581506071724!5m2!1sru!2sru"
+ width="700"height="340" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+</div>
+</body>
+<?php require'footer.php';?>
+</html>
